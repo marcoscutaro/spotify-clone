f98253ba3995d8d5e1c97a00ade081b214f3e90c
marcoscutaro <marcostruggia@gmail.com>
2023-10-31 00:59:38 +0100

    dots

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
-import { Figtree } from 'next/font/google'
+import { Figtree } from 'next/font/google';
 
-import getSongsByUserId from '@/actions/getSongsByUserId'
-import getActiveProductsWithPrices from '@/actions/getActiveProductsWithPrices'
-import Sidebar from '@/components/Sidebar'
-import ToasterProvider from '@/providers/ToasterProvider'
-import UserProvider from '@/providers/UserProvider'
-import ModalProvider from '@/providers/ModalProvider'
-import SupabaseProvider from '@/providers/SupabaseProvider'
-import Player from '@/components/Player'
+import getSongsByUserId from '@/actions/getSongsByUserId';
+import getActiveProductsWithPrices from '@/actions/getActiveProductsWithPrices';
+import Sidebar from '@/components/Sidebar';
+import ToasterProvider from '@/providers/ToasterProvider';
+import UserProvider from '@/providers/UserProvider';
+import ModalProvider from '@/providers/ModalProvider';
+import SupabaseProvider from '@/providers/SupabaseProvider';
+import Player from '@/components/Player';
 
-import './globals.css'
+import './globals.css';
 
 const font = Figtree({ subsets: ['latin'] })
 
